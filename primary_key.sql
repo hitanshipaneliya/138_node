e2e04f8a03b840e5abfcaee866f6fa13053f729e
hitanshipaneliya <114410037+hitanshipaneliya@users.noreply.github.com>
2022-10-12 18:17:59 +0530

    primary key concept

---
+++ primary_key.sql
@@ -0,0 +1,9 @@
+-- create table categories(
+--     cid int(10),
+--     cname varchar(100),
+--     constraint pk_categories primary key(cid)
+-- );
+
+insert into categories values(10,"speaker");
+insert into categories values(20,"pen-drive");
+insert into categories values(20,"pen-drive");
